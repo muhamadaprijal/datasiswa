da7eb85083efce4b62d362365b3003891f8c7f92
muhamadaprijal <aprijalmaprijal32@gmail.com>
2024-02-12 09:00:22 +0700

    menambakan daptar hapus

--- main.js
+++ main.js
@@ -46,3 +46,7 @@ export async function tambahSiswa(val) {
       console.log('Error menambah dokumen: ' + e);
     }
  }
+ 
+ export async function hapussiswa(docld){
+   await deleteDoc(doc(db,"siswa"),docld);
+ }
